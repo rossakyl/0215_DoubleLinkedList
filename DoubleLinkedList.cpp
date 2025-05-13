6479c16e2a632e85556247181b6e09950d125646
rossakyl <rossakayla01@gmail.com>
2025-05-13 12:10:09 +0700

    pembuatan function search untuk mencari data

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -4,17 +4,16 @@ using namespace std;
 // membuat struktur untuk object double linkedlist
 struct Node
 {
-     // deklarasi noMhs dan name untuk menampung data
+    // deklarasi noMhs dan name untuk menampung data
     int noMhs;
     string name;
-      // deklarasi pointer next dan pref untuk penunjukkan data sebelum dan sesudah
+    // deklarasi pointer next dan prev untuk petunjuk ke data sebelum dan sesudahnya
     Node *next;
     Node *prev;
-
 };
 
 // deklarasi pointer START dan pemberian nilai
-Node*START=NULL;
+Node *START = NULL;
 
 // deklarasi prosedur addNode
 void addNode()
@@ -34,34 +33,73 @@ void addNode()
         // kondisi jika star tidak kosong dan noMhs node baru sama dengan noMhs start
         if (START != NULL && newNode->noMhs == START->noMhs)
         {
-            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            cout << "\033[31mDuplicate roll number not allowed\033[0m" << endl;
             return;
         }
-        //if the list is empety, make the new node the Start
-        //jika list kosong, maka node text nya adalah start 
-        newNode->next = START; // Step3; make the new code to the first
+        // if the list is empty, make the new node the START
+        // jika list kosong, maka node next nya adalah START
+        newNode->next = START; // step 3: make the new node point to first node
         // kondisi jika start tidak memiliki nilai atau tidak kosong
         if (START != NULL)
         {
-            START->prev = newNode; // step 4 : make the first mode point to the new node
+            START->prev = newNode; // step 4: make the first node point to the new node
         }
-
-        //memberi nilai prev = null dan start = node baru
-        newNode->prev = NULL; // step 5: make the new node point to NULL
-        START = newNode; // step 6: make the new node the first node
+        // memberi nilai prev = null dan start = node baru
+        newNode->prev = NULL; // step 5: make the new node point to null
+        START = newNode;      // step 6: make the new node the first node
     }
-    //kondisi jika semua kondisi if tidak terpenuhi
+    // kondisi jika semua kondisi if tidak terpenuhi
     else
     {
+        // insert the new node in the middle or at the end
+        // set nilai current = start dan nilai previous = null
+        Node *current = START; // step 1.a: start from the first node
+        Node *previous = NULL; // step 1.b: previous node is null initially
+
+        // looping selama current != null dan noMhs dari current lebih kecil dari noMhs node baru
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {                            // step 1.c: traverse the list to find the correct position
+            previous = current;      // step 1.d: move the previous to the current node
+            current = current->next; // step 1.e: move the current to the next node
+        }
+
+        // set nilai next node baru = current dan prev node baru = previous
+        newNode->next = current;  // step 4: make the next field of the new node point to the current node
+        newNode->prev = previous; // step 5: make the previous field of the new node point to the previous node
+
+        // kondisi jika current tidak sama dengan null
+        if (current != NULL)
+        {
+            current->prev = newNode; // step 6: make the previous field of the current node point to the new node
+        }
+
+        // kondisi jika previous tidak sama dengan null
+        if (previous != NULL)
+        {
+            previous->next = newNode; // step 7: make the next field of the previous node point to the new node
+        }
+        // kondisi jika if previous sama dengan null
+        else
+        {
+            // if previous is still null, it means newNode is now the first node
+            START = newNode;
+        }
+    }
+}
+
+// pembuatan function search umtuk mencari data
+bool search(int rollNo, Node **previous, Node **current)
+{
+    *previous = NULL;
+    *current = START;
+    while (*current != NULL && (*current)->noMhs != rollNo)
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+    return (*current != NULL);
+}
+
+
+ 
         
-
-        // insert the new node in the midlle or at the end
-        // set nilai current = start dan nilai previos = null
-        Node *current = START; // STEP 1.a: start from the first mode
-        Node *previous = NULL; // STEP 1.b: previous node is NULL Inittialy
-
-      
-    }       
-}    
-
-
