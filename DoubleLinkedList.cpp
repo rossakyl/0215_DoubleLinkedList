6133bda42cebc83540586a040e13077bfb7f67af
rossakyl <rossakayla01@gmail.com>
2025-05-13 10:07:39 +0700

    deklarasi noMhs dan name untuk menampung data

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -4,5 +4,7 @@ using namespace std;
 // membuat struktur untuk object double linkedlist
 struct Node
 {
-
+     // deklarasi noMhs dan name untuk menampung data
+    int noMhs;
+    string name;
 };
